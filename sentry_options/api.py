e34ea1303a7bfbebbb45097f96ea3ceaf1591188
anthony sottile <anthony.sottile@sentry.io>
2025-08-20 16:23:27 -0400

    implement stat-based reloading

--- sentry_options/api.py
+++ sentry_options/api.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import json
+import os.path
 from typing import Any
 from typing import LiteralString
 
@@ -14,8 +16,35 @@ _groups = {
 
 
 class OptionGroup[Name]:
+    def __init__(
+            self,
+            name: str,
+            *,
+            _location: str = '/etc/sentry-options',
+    ) -> None:
+        self.name = name
+        self._definitions = _groups[self.name]
+        self._location = _location
+        self._inode = -1
+        self._data: Any = {}
+
+    def __repr__(self) -> str:
+        return f'{type(self).__name__}({self.name!r})'
+
+    def _ensure_data(self) -> None:
+        loc = os.path.join(self._location, self.name, 'values.json')
+        res = os.stat(loc)
+        if self._inode == res.st_ino:
+            return
+        else:
+            with open(loc, encoding='UTF-8') as f:
+                self._data = json.load(f)
+                self._inode = os.fstat(f.fileno()).st_ino
+
     def get(self, key: LiteralString) -> object:
-        raise NotImplementedError
+        self._ensure_data()
+        option = self._definitions[key]
+        return self._data.get(key, option.default)
 
 
 def option_group(group: LiteralString) -> OptionGroup[Any]:
---
+++ tests/api_test.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+import builtins
+import pathlib
+from typing import Literal
+from unittest import mock
+
+from sentry_options.api import OptionGroup
+
+
+def test_reloading(tmp_path: pathlib.Path) -> None:
+    tdir = tmp_path.joinpath('testing')
+    tdir.mkdir()
+    tdir.joinpath('values.json').write_text('{}')
+
+    g: OptionGroup[Literal['testing']]
+    g = OptionGroup('testing', _location=str(tmp_path))
+    # should have the default value
+    assert g.get('example-option') == ''
+
+    # replace the values file
+    tdir.joinpath('values.json.tmp').write_text('{"example-option": "hi"}')
+    tdir.joinpath('values.json.tmp').rename(tdir.joinpath('values.json'))
+
+    # should get the fresh value
+    assert g.get('example-option') == 'hi'
+    # should not attempt to reload
+    with mock.patch.object(builtins, 'open', side_effect=OSError):
+        assert g.get('example-option') == 'hi'
