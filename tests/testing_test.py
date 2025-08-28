8524af198b0c9e0b9fb04ac7361490a627895b3c
anthony sottile <anthony.sottile@sentry.io>
2025-08-28 15:02:59 -0400

    add override_options testing helper

---
+++ tests/testing_test.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+
+import pathlib
+
+from sentry_options.testing import override_options
+from testing.option_group import empty_option_group
+
+
+def test_override_options_simple(tmp_path: pathlib.Path) -> None:
+    g = empty_option_group(tmp_path)
+    assert g.get('example-option') == ''
+    with override_options(testing={'example-option': '1'}):
+        assert g.get('example-option') == '1'
+    assert g.get('example-option') == ''
+
+
+def test_override_options_nested(tmp_path: pathlib.Path) -> None:
+    g = empty_option_group(tmp_path)
+    assert g.get('example-option') == ''
+    with override_options(testing={'example-option': '1'}):
+        assert g.get('example-option') == '1'
+        with override_options(testing={'example-option': '2'}):
+            assert g.get('example-option') == '2'
+        assert g.get('example-option') == '1'
+    assert g.get('example-option') == ''
+
+
+def test_override_options_decorator(tmp_path: pathlib.Path) -> None:
+    g = empty_option_group(tmp_path)
+
+    @override_options(testing={'example-option': '1'})
+    def func(x: int) -> str:
+        return f'{x}:{g.get("example-option")}'
+
+    assert func(2) == '2:1'
