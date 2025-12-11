e2c5547557bf860b23fe75c5156abb4cf52a9ec8
kenzoengineer <kenzoengineer@gmail.com>
2025-12-11 13:42:51 -0800

    use new options init

--- example/rust/src/main.rs
+++ example/rust/src/main.rs
@@ -1,6 +1,6 @@
 use std::{thread::sleep, time::Duration};
 
-use sentry_options::Options;
+use sentry_options::{init, options};
 
 /// An example usage of the Rust options client library
 /// Every 3 seconds, prints out the value of example-option, float-option, and bool-option
@@ -8,13 +8,14 @@ use sentry_options::Options;
 /// Updating values in `../values` will be reflected in stdout
 /// ^C to exit
 fn main() -> anyhow::Result<()> {
-    let options = Options::new()?;
+    let _ = init();
+    let sentry_options = options("testing");
 
     loop {
         sleep(Duration::from_secs(3));
-        let string_value = options.get("testing", "example-option")?;
-        let float_value = options.get("testing", "float-option")?;
-        let bool_value = options.get("testing", "bool-option")?;
+        let string_value = sentry_options.get("example-option")?;
+        let float_value = sentry_options.get("float-option")?;
+        let bool_value = sentry_options.get("bool-option")?;
         println!(
             "values: {} | {} | {}",
             string_value, float_value, bool_value
