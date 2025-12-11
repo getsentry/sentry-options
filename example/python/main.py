389728e80e9507113ae59449efbba4950ba4f8f2
Hubert Deng <hubertdeng123@gmail.com>
2025-12-11 12:47:37 -0800

    initial python client

---
+++ example/python/main.py
@@ -0,0 +1,24 @@
+"""An example usage of the Python options client library.
+
+Every 3 seconds, prints out the value of example-option, float-option,
+and bool-option.
+
+Updating values in `../values` will be reflected in stdout.
+Ctrl+C to exit.
+"""
+from __future__ import annotations
+
+import time
+
+from sentry_options_new import init
+from sentry_options_new import options
+
+init()
+testing = options('testing')
+
+while True:
+    time.sleep(3)
+    string_value = testing.get('example-option')
+    float_value = testing.get('float-option')
+    bool_value = testing.get('bool-option')
+    print(f"values: {string_value} | {float_value} | {bool_value}", flush=True)
