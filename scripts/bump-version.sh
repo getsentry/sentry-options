c0593fc6ad63bf77b00af5e71976f217e2e88fd4
kenzoengineer <kenzoengineer@gmail.com>
2026-01-29 14:16:52 -0800

    add version bumping by default to that script

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -8,5 +8,6 @@ cd "$(dirname "${BASH_SOURCE[0]}")/.."
 perl -i -pe "s/^version = \".*\"/version = \"$NEW_VERSION\"/" clients/python/pyproject.toml
 perl -i -pe "s/^version = \".*\"/version = \"${NEW_VERSION}\"/" Cargo.toml
 perl -i -pe "s/(sentry-options-validation = \{ path = \"[^\"]*\", version = \")[^\"]*/\${1}${NEW_VERSION}/" Cargo.toml
+perl -i -pe "if (/cli-version:/ .. /default:/) { s/(default: \")[0-9.]+\"/\${1}${NEW_VERSION}\"/ }" .github/actions/validate-schema/action.yml
 
 cargo metadata --format-version 1 > /dev/null
