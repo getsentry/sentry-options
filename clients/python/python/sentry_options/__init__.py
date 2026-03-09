1c27e9dbaed9219a617397c2a53c0473c27d95c1
Hubert Deng <hubertdeng123@gmail.com>
2026-03-09 15:26:44 -0700

    fix

--- clients/python/python/sentry_options/__init__.py
+++ clients/python/python/sentry_options/__init__.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from typing import Union
+
 from sentry_options._core import init
 from sentry_options._core import InitializationError
 from sentry_options._core import NamespaceOptions
@@ -9,11 +11,16 @@ from sentry_options._core import SchemaError
 from sentry_options._core import UnknownNamespaceError
 from sentry_options._core import UnknownOptionError
 
+_Primitive = Union[str, int, float, bool]
+_Object = dict[str, _Primitive]
+OptionValue = Union[_Primitive, _Object, list[Union[_Primitive, _Object]]]
+
 __all__ = [
     'init',
     'InitializationError',
     'NamespaceOptions',
     'options',
+    'OptionValue',
     'OptionsError',
     'SchemaError',
     'UnknownNamespaceError',
